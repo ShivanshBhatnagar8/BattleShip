df7a0eccc7261ae1235056fd2e44b66a7be26c9e
ShivanshBhatnagar8 <shivanshbhatnagar8@gmail.com>
2024-04-03 22:49:40 +0530

    Added logic to determine Winner and making game fully playable

--- src/controller/gameLogic.js
+++ src/controller/gameLogic.js
@@ -4,10 +4,12 @@ import { Player } from "../model/player";
 import {
   getPlayerGrid,
   getComputerGrid,
-  getActivePlayer,
+  getWinnerName,
   getOrientationButton,
   getResetButton,
 } from "./Dom";
+
+let haveWinner = false;
 
 let human = new Player("human");
 let computer = new Player("computer");
@@ -45,25 +47,6 @@ function placementOfShipInUi(array, box) {
     }
   });
 }
-// function placementOfShipByComputer(array, grid, box) {
-//   console.log(array);
-//   for (let i = 0; i < array.length; i++) {
-//     for (let j = 0; j < array[i].length; j++) {
-//       if (array[i][j] === "x") {
-//         const cell = document.querySelector(
-//           `.${box}[data-x="${i}"][data-y="${j}"]`
-//         );
-//         cell.style.backgroundColor = "red";
-//       } else {
-//         const cell = document.querySelector(
-//           `.${box}[data-x="${i}"][data-y="${j}"]`
-//         );
-//         cell.style.backgroundColor = "blue";
-//       }
-//     }
-//     break;
-//   }
-// }
 
 function renderShipOnGrid(ship) {
   const userGrids = getPlayerGrid();
@@ -73,23 +56,16 @@ function renderShipOnGrid(ship) {
       const position = positionBtn.textContent;
       const x = el.getAttribute("data-x");
       const y = el.getAttribute("data-y");
-      //console.log(ship);
       if (!ship.isPlaced) {
         const result = human.placementOfShipByPlayer(
           ship,
           [Number(x), Number(y)],
           position.toLowerCase()
         );
-        console.log(ship);
         if (result !== undefined) {
-          console.log(ship.isPlaced);
-          console.log(result);
           Ship.shipCount++;
-          console.log(Ship.shipCount);
           placementOfShipInUi(result, "user-box");
           if (Ship.shipCount === 5) {
-            console.log(Ship.shipCount);
-            console.log(human.playerBoard.board);
             attackLogic();
           }
         }
@@ -102,20 +78,14 @@ function renderShipOnGridByComputer(ship) {
   const computerGrids = getComputerGrid();
   computerGrids.forEach(() => {
     if (!ship.isPlaced) {
-      const result = computer.placementOfShipByPlayer(ship);
-      if (result !== undefined) {
-        placementOfShipInUi(result, "computer-box");
-        // Player.switchPlayerTurn();
-      }
+      computer.placementOfShipByPlayer(ship);
     }
   });
 }
 
 function humanAttack(board, grids, box) {
   grids.forEach((el) => {
-    console.log(el.style.backgroundColor);
     el.addEventListener("click", function () {
-      console.log("hello");
       const x = el.getAttribute("data-x");
       const y = el.getAttribute("data-y");
       const cell = document.querySelector(
@@ -124,15 +94,12 @@ function humanAttack(board, grids, box) {
       if (
         Player.activePlayer === "human" &&
         cell.style.backgroundColor !== "red" &&
-        cell.style.backgroundColor !== "blue"
+        cell.style.backgroundColor !== "blue" &&
+        !haveWinner
       ) {
         const result = human.shipAttack(board, [Number(x), Number(y)]);
-        console.log(x, y);
-        console.log("it run");
-        console.log(result);
-        console.log(Player.activePlayer);
+        checkingIfShipAttacked([Number(x), Number(y)], "computer");
         if (result && result[Number(x)][Number(y)] !== null) {
-          console.log(result[Number(x)][Number(y)]);
           el.style.backgroundColor = "red";
           Player.switchPlayerTurn();
           attackLogic();
@@ -141,48 +108,53 @@ function humanAttack(board, grids, box) {
           Player.switchPlayerTurn();
           attackLogic();
         }
-        console.log(Player.activePlayer);
       }
     });
   });
 }
 
-function computerAttack(board, grids, box) {
+function computerAttack(board, box) {
   if (Player.activePlayer === "computer") {
     const result = computer.shipAttack(board);
-    console.log(result);
+    checkingIfShipAttacked(result, "human");
     const [x, y] = result;
+
     const cell = document.querySelector(
       `.${box}[data-x="${x}"][data-y="${y}"]`
     );
-    console.log("computer run");
-    if (board[x][y] === "x") {
+    if (board[x][y] !== null) {
       cell.style.backgroundColor = "red";
-      console.log(cell.getAttribute("data-x"));
-      console.log(cell.getAttribute("data-y"));
     } else {
       cell.style.backgroundColor = "blue";
-      console.log(cell.getAttribute("data-x"));
-      console.log(cell.getAttribute("data-y"));
     }
-    console.log(board);
   }
   Player.switchPlayerTurn();
 }
 
-function attackLogic() {
-  const userGrids = getPlayerGrid();
-  const computerGrids = getComputerGrid();
-  if (Player.activePlayer === "human") {
-    humanAttack(computer.playerBoard.board, computerGrids, "computer-box");
-    //console.log(Player.activePlayer);
-    //Player.switchPlayerTurn();
-  } else {
-    computerAttack(human.playerBoard.board, userGrids, "user-box");
+function checkingIfShipAttacked(coords, opponent) {
+  if (opponent === "human") {
+    const shipHolder = [carrier, battleship, cruiser, destroyer, submarine];
+    shipHolder.forEach((el) => {
+      human.playerBoard.receiveAttack(el, coords);
+    });
 
-    //console.log(Player.activePlayer);
+    winnerLogic(shipHolder, opponent);
+  } else if (opponent === "computer") {
+    const shipHolder = [
+      carrierAi,
+      battleshipAi,
+      cruiserAi,
+      destroyerAi,
+      submarineAi,
+    ];
+    shipHolder.forEach((el) => {
+      computer.playerBoard.receiveAttack(el, coords);
+    });
+
+    winnerLogic(shipHolder, opponent);
   }
 }
+
 function changeOrientation() {
   const orientationBtn = getOrientationButton();
   orientationBtn.addEventListener("click", function () {
@@ -208,7 +180,27 @@ function placeShipByComputer(ship) {
     return renderShipOnGridByComputer(ship);
   }
 }
-
+function attackLogic() {
+  const computerGrids = getComputerGrid();
+  if (Player.activePlayer === "human") {
+    humanAttack(computer.playerBoard.board, computerGrids, "computer-box");
+  } else {
+    computerAttack(human.playerBoard.board, "user-box");
+  }
+}
+function winnerLogic(array, opponent) {
+  const winner = getWinnerName();
+  const isWinner = array.every((el) => {
+    return el.isShipSunk();
+  });
+  if (isWinner && opponent === "computer") {
+    winner.textContent = "YOU WON!";
+    haveWinner = true;
+  } else if (isWinner && opponent === "human") {
+    winner.textContent = "COMPUTER WON!";
+    haveWinner = true;
+  }
+}
 function resetGame() {
   const gameResetButton = getResetButton();
   gameResetButton.addEventListener("click", function () {
